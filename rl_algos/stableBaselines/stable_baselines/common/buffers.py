8645b4be3e3013777ffb3ac0fcb7420d73648179
Tarang <tarang.sriv@berkeley.edu>
2021-03-03 14:04:34 -0500

    wip to implement gaussian buffer with log prob

--- rl_algos/stableBaselines/stable_baselines/common/buffers.py
+++ rl_algos/stableBaselines/stable_baselines/common/buffers.py
@@ -262,3 +262,70 @@ class PrioritizedReplayBuffer(ReplayBuffer):
         self._it_min[idxes] = priorities ** self._alpha
 
         self._max_priority = max(self._max_priority, np.max(priorities))
+
+
+
+# class GaussianBufferIncremental(BaseBuffer):
+class GaussianBufferIncremental():
+    def __init__(self, obs_dim):
+        super().__init__()
+        self.obs_dim = obs_dim
+        self.reset()
+        
+    def inserts(self):
+        return self.buffer_size
+    
+    def add(self, state):
+        if (self.inserts() == 0):
+             self._state_mean = state
+             self._state_var = np.ones_like(state)
+        else:
+            x_mean_old = self._state_mean
+            self._state_mean = self._state_mean + ((state - self._state_mean)/self.inserts())
+            
+        if (self.inserts() == 2):
+            self._state_var = (state - ((self._prev_state[0]+state)/2.0)**2)/2.0
+        elif (self.inserts() > 2):
+            self._state_var = (((self.inserts()-2)*self._state_var) + ((self.inserts()-1)*(x_mean_old - self._state_mean)**2) + ((state - self._state_mean)**2))
+            self._state_var = (self._state_var/float(self.inserts()-1))
+            
+        self._prev_state = state
+        self._state_var = np.fabs(self._state_var)
+#         print ("self._state_var: ", np.sqrt(self._state_var))
+        self.buffer_size += 1
+
+    def get_params(self):
+        #import pdb; pdb.set_trace()
+        means = self._state_mean
+        stds = np.sqrt(self._state_var)
+        params = np.concatenate([means, stds])
+        return params
+
+    def logprob(self, obs):
+        obs = obs.copy()
+        means = self._state_mean
+        stds = np.sqrt(self._state_var)
+        
+        # For numerical stability, clip stds to not be 0
+        thresh = 1e-3
+#         print ("thresh: ", thresh)
+        stds = np.clip(stds, thresh, None)
+#         print ("stds, means: ", np.mean(stds), np.mean(means))
+#         import pdb; pdb.set_trace()
+        # Gaussian log prob
+        logprob = -0.5*np.sum(np.log(2*np.pi*stds)) - np.sum(np.square(obs-means)/(2*np.square(stds)))
+        return logprob
+
+    def reset(self):
+        self._state_mean =  np.zeros(self.obs_dim)
+        self._state_var = np.ones(self.obs_dim)
+        self.buffer_size = 0
+        
+    def entropy(self):
+#         stds = np.sqrt(self._state_var)
+        # For numerical stability, clip stds to not be 0
+        thresh = 1e-3
+        var = np.clip(self._state_var, thresh, None)
+        return -0.5*np.sum(np.log(2*np.pi*np.e*var))
+        
+        
