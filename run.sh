b3d4aa95ad18292efb5029aa215ae9ba5a626597
Lucas <lucas.ingamba@gmail.com>
2021-04-14 12:35:06 -0700

    edits to run command

--- run.sh
+++ run.sh
@@ -4,10 +4,7 @@ UNAME=tc
 DOCKER_BUILDKIT=1 docker build . -t tc-temp --build-arg UID=$(id -u) --build-arg UNAME=$UNAME
 WORKDIR=/home/$UNAME
 
-GPU_FLAGS=""
-if command -v nvidia-smi &> /dev/null
-then
-  GPU_FLAGS="--gpus=all"
-fi
-
-docker run -it $GPU_FLAGS -p 5000:5000 --rm --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 tc-temp #-v "$(pwd):$WORKDIR" tc-temp
+docker run \
+    -p 5000:5000 \
+    -v "$(pwd)/database:/app/database" \
+    lucasspangher/social_game_api_image:latest
