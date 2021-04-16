8b14edf60b07b2f1ce4eb4811579140bdecd5a70
Lucas <lucas.ingamba@gmail.com>
2021-04-15 18:08:56 -0700

    adding database folder etc.

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-DATABASE_CONNECTION_URI = "sqlite:///database.db"
+DATABASE_CONNECTION_URI = "sqlite:///database/database.db"
 SQUARE_WAVE_DATA_PATH = "square_waves_two_weeks.csv"
 BASE_POINTS_VALUE = 1000
 
@@ -10,3 +10,4 @@ START_OF_DAY_HOUR = 6
 END_OF_DAY_HOUR = 23
 
 IS_MULTIAGENT = False
+
