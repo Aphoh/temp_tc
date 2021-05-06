f87d5aeb11b08ed52c49a0333cc27f139aaabbaa
lucas_spangher <lucas.ingamba@gmail.com>
2021-05-05 22:11:38 -0700

    adding running instructions

---
+++ example_run_microgrids.sh
@@ -0,0 +1,3 @@
+python3 rl_algos/StableBaselines.py --num_steps=300000 --exp_name=pm_net_exports --algo=ppo --library=rllib --one_day=15 --energy_in_state=T --price_in_state=F --gym_env=microgrid -w --reward_function=profit_maximizing
+
+python3 rl_algos/StableBaselines.py --num_steps=300000 --exp_name=ms_net_exports --algo=ppo --library=rllib --one_day=15 --energy_in_state=T --price_in_state=F --gym_env=microgrid -w --reward_function=market_solving
