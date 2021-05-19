e1e08b1e2b66e47f192c3ba873ecd82995c43522
Demosthen <1008981@gmail.com>
2021-05-19 09:57:56 -0700

    Forgot to add dataset script in last commit

--- rl_algos/build_dataset.sh
+++ rl_algos/build_dataset.sh
@@ -3,14 +3,15 @@ RESPONSE_TYPES='l t s'
 
 for RESPONSE in $RESPONSE_TYPES
 do 
-    for PM in 10 15 20
+    for A in {1..200}
     do
+        PM=($RANDOM % 10 + 10)
         echo $PM $RESPONSE
-        python StableBaselines.py --algo=sac --library=rllib -w --num_steps=100 --person_type_string=d --response_type_string=$RESPONSE --points_multiplier=$PM
+        python StableBaselines.py --algo=sac --library=rllib  --num_steps=5 --person_type_string=d --response_type_string=$RESPONSE --points_multiplier=$PM --checkpoint_interval=-1
     done
 done
-for I in {1..3}
-do
-    echo $I
-    python StableBaselines.py --algo=sac --library=rllib -w --num_steps=100 --points_multiplier=$PM --person_type_string=c
-done
+# for I in {1..3}
+# do
+#     echo $I
+#     python StableBaselines.py --algo=sac --library=rllib -w --num_steps=100 --points_multiplier=$PM --person_type_string=c
+# done
