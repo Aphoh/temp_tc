d2ae3cad8be0ee67364f365e62f24b99e161f647
Tarang <tarang.sriv@berkeley.edu>
2021-05-19 14:04:08 -0400

    removed gpus to use savio2

--- singularity_planning.sh
+++ singularity_planning.sh
@@ -11,14 +11,8 @@
 # Number of nodes:
 #SBATCH --nodes=1
 #
-# Number of tasks (one for each GPU desired for use case) (example):
-#SBATCH --ntasks=1
-#
 # Processors per task (please always specify the total number of processors twice the number of GPUs):
 #SBATCH --cpus-per-task=2
-#
-#Number of GPUs, this can be in the format of "gpu:[1-4]", or "gpu:K80:[1-4] with the type included
-#SBATCH --gres=gpu:1
 #
 # Wall clock limit (2hrs):
 #SBATCH --time=01:00:00
