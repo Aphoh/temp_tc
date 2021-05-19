c79823e01bb29ea972dde7c365b155dd6cd544e3
Tarang Srivastava <tarangsriv@ln002.brc>
2021-05-19 10:57:27 -0700

    added changes for planning

--- planning_run.sh
+++ planning_run.sh
@@ -1,3 +1,4 @@
+export WANDB_API_KEY=3cc9567683fdfd761e3a28516c67a053e4b76331
 export WANDB_RUN_GROUP=testing
 
 python3 rl_algos/StableBaselines.py -w --num_steps=300000 --exp_name=planning_test --algo=ppo --library=rllib --one_day=15 --energy_in_state=T --price_in_state=F --gym_env=planning 
