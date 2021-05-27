e5b53405c9f83e309c2e1f3d2f231fe7c14fe6f1
Tarang <tarang.sriv@berkeley.edu>
2021-05-27 03:13:02 -0400

    trying fix again

--- example_run.sh
+++ example_run.sh
@@ -1,6 +1,4 @@
 #python3 rl_algos/StableBaselines.py --num_steps=300000 -w --exp_name=maml_smirl_ppo --algo=ppo --library=rllib --one_day=15 --smirl_weight=0.10 --energy_in_state=T --price_in_state=F --checkpoint=rl_algos/maml_ckpts/vivid-pyramid-501100.ckpt 
-mkdir /usr/bin/nvidia-smi
-mkdir /usr/share/zoneinfo/Etc/UTC
 python3 rl_algos/StableBaselines.py --num_steps=200000 -w --exp_name=baseline_ppo --algo=ppo --library=rllib --one_day=15 --energy_in_state=T --price_in_state=F 
 # python3 rl_algos/StableBaselines.py --num_steps=200000 -w --exp_name=smirl_10_ppo --algo=ppo --library=rllib --one_day=15 --smirl_weight=0.10 --energy_in_state=T --price_in_state=F 
 
--- singularity_preamble.sh
+++ singularity_preamble.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+mkdir /usr/bin/nvidia-smi
+mkdir /usr/share/zoneinfo/Etc/UTC
 pip install --user --upgrade pip
 pip install -r ./requirements.txt
 pip install -e ./gym-socialgame/
