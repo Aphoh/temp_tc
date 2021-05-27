b5e792e957bc09f20f3369d5ddd26c1641527eb0
Tarang <tarang.sriv@berkeley.edu>
2021-05-27 03:45:43 -0400

    changes to user dfault

--- singularity_preamble.sh
+++ singularity_preamble.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-# pip install --user --upgrade pip
-
+python3 -m pip install --upgrade pip
 python -m pip install -r ./requirements.txt
 python -m pip install -e ./gym-socialgame/
 python -m pip install -e ./gym-microgrid/
