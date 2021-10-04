16c91eb49c945e695a3a8a72f65fdcc48ee0bb22
Tarang Srivastava <tarang.sriv@berkeley.edu>
2021-10-04 10:20:21 +0000

    added key since savio wont let me change file

--- example_run.sh
+++ example_run.sh
@@ -1,3 +1,4 @@
+export WANDB_API_KEY="3cc9567683fdfd761e3a28516c67a053e4b76331"
 export WANDB_RUN_GROUP="smirl_run_v2"
 
 python3 rl_algos/StableBaselines.py -w --num_steps=300000 --exp_name=smirl_010_ppo --algo=ppo --library=rllib --one_day=15 --smirl_weight=0.10 --energy_in_state=T --price_in_state=F
