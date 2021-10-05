179c3dc14d36814766e5ff8bfe90b539b0d7cee1
lucas_spangher <lucas.ingamba@gmail.com>
2021-10-05 16:57:38 -0700

    formatting commands

--- curiosity_command.py
+++ curiosity_command.py
@@ -16,8 +16,8 @@ parser.add_argument('--steps',
 
 args = parser.parse_args()
 
-rew = {#0: "curiosity_mean",
-    #1: "curiosity_l2_norm",
+rew = {0: "curiosity_mean",
+    1: "curiosity_l2_norm",
     2: "apt",
     3: "curiosity_max",
     4: "control"
@@ -25,6 +25,6 @@ rew = {#0: "curiosity_mean",
 
 # IPython.embed()
 
-command = "python rl_algos/StableBaselines.py -w --library=rllib --num_steps=10000 --algo=ppo --gym_env=curiosity --base_log_dir=./logs/ --planning_steps=0  --planning_model=ANN --planning_ckpt=planning_ckpts_diverse2/100000/epoch=100-step=4949.ckpt --intrinsic_rew={} --total_intrinsic_steps={} --exp_name=socialgame".format(rew[args.intrinsic_rew], args.steps)
+command = "python rl_algos/StableBaselines.py -w --library=rllib --num_steps=10000 --energy_in_state=T  --algo=ppo --gym_env=curiosity --base_log_dir=./logs/ --planning_steps=0  --planning_model=ANN --planning_ckpt=planning_ckpts_diverse2/100000/epoch=100-step=4949.ckpt --intrinsic_rew={} --total_intrinsic_steps={} --exp_name=socialgame".format(rew[args.intrinsic_rew], args.steps)
 
 os.system(command)
--- intrinsic_curiosity_experiment.sh
+++ intrinsic_curiosity_experiment.sh
@@ -1,6 +1,6 @@
 for steps in 50 5000 
 do
-for rew in 4 
+for rew in 0 1 2 3 4 
 do
     python curiosity_command.py --intrinsic_rew=$rew --steps=$steps
 done
