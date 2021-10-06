dba9f4b698476fe575c034063531c7ac7e56295e
Lucas <lucas.ingamba@gmail.com>
2021-10-05 23:35:08 -0700

    fixing intr_extr 3

--- curiosity_command.py
+++ curiosity_command.py
@@ -45,18 +45,6 @@ command16 = "python rl_algos/StableBaselines.py -w --library=rllib --num_steps=5
 command17 = "python rl_algos/StableBaselines.py -w --library=rllib --num_steps=50000 --algo=ppo --gym_env=curiosity --energy_in_state=T --base_log_dir=./logs/ --planning_steps=0  --planning_model=ANN --planning_ckpt=planning_ckpts_diverse2/100000/epoch=100-step=4949.ckpt --intrinsic_rew={} --total_intrinsic_steps={} --exp_name=socialgame ".format(rew[6], 5000)
 
 
-os.system(command1)
-os.system(command2)
-os.system(command3)
-os.system(command4)
-os.system(command5)
-os.system(command6)
-os.system(command7)
+Command = command1 + command2 + command3 + command4 + command5 + command6 + command7 + command11 + command12 + command13 + command14 + command15 + command16 + command17 
 
-os.system(command11)
-os.system(command12)
-os.system(command13)
-os.system(command14)
-os.system(command15)
-os.system(command16)
-os.system(command17)
+os.system(Command)
